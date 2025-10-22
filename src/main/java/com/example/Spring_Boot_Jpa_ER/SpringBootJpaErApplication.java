c790a36bdee20c6665aeb26d335b344a397684d0
emyca <evmyca@gmail.com>
2025-10-22 11:05:50 +0300

    Initial commit

---
+++ src/main/java/com/example/Spring_Boot_Jpa_ER/SpringBootJpaErApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_Jpa_ER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaErApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaErApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring_Boot_Jpa_ER/SpringBootJpaErApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_Jpa_ER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaErApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
