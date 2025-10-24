1bb98fe8ed2c8130e554420c9ba850f14210332a
emyca <evmyca@gmail.com>
2025-10-24 12:25:18 +0300

    Revert "Add repositories"
    This reverts commit 3affd8397fab7b7eb9afb807400bd63ca2272ac7.

--- src/main/java/com/example/Spring_Boot_Jpa_ER/_01_1_to_1_unidir_fk_jc/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Spring_Boot_Jpa_ER._01_1_to_1_unidir_fk_jc.repository;
-
-import com.example.Spring_Boot_Jpa_ER._01_1_to_1_unidir_fk_jc.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- src/main/java/com/example/Spring_Boot_Jpa_ER/_01_1_to_1_unidir_fk_jc/repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Spring_Boot_Jpa_ER._01_1_to_1_unidir_fk_jc.repository;
-
-import com.example.Spring_Boot_Jpa_ER._01_1_to_1_unidir_fk_jc.entity.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
