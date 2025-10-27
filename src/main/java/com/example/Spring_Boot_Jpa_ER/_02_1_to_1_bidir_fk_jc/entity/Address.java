ff6c7312695ca1d97ade64f1ba8b1e98ba7d832c
emyca <evmyca@gmail.com>
2025-10-27 09:33:55 +0200

    Add feature entities

---
+++ src/main/java/com/example/Spring_Boot_Jpa_ER/_02_1_to_1_bidir_fk_jc/entity/Address.java
@@ -0,0 +1,32 @@
+package com.example.Spring_Boot_Jpa_ER._02_1_to_1_bidir_fk_jc.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity(name = "addresses")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "street")
+    private String street;
+    @Column(name = "building")
+    private String building;
+    @Column(name = "apartment")
+    private String apartment;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "address")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Buyer buyer;
+}
+
---
+++ src/main/java/com/example/Spring_Boot_Jpa_ER/_02_1_to_1_bidir_fk_jc/entity/Buyer.java
@@ -0,0 +1,29 @@
+package com.example.Spring_Boot_Jpa_ER._02_1_to_1_bidir_fk_jc.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity(name = "buyers")
+public class Buyer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
+}
+
