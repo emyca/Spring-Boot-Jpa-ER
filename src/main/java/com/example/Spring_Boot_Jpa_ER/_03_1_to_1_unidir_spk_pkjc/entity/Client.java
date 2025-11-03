5552ecdd58d6b75523f36af712309a82aa490450
emyca <evmyca@gmail.com>
2025-11-03 11:08:24 +0200

    Add entities

---
+++ src/main/java/com/example/Spring_Boot_Jpa_ER/_03_1_to_1_unidir_spk_pkjc/entity/Client.java
@@ -0,0 +1,28 @@
+package com.example.Spring_Boot_Jpa_ER._03_1_to_1_unidir_spk_pkjc.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @OneToOne(cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
+    private Residence residence;
+}
---
+++ src/main/java/com/example/Spring_Boot_Jpa_ER/_03_1_to_1_unidir_spk_pkjc/entity/Residence.java
@@ -0,0 +1,27 @@
+package com.example.Spring_Boot_Jpa_ER._03_1_to_1_unidir_spk_pkjc.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity(name = "residences")
+public class Residence {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "street")
+    private String street;
+    @Column(name = "building")
+    private String building;
+    @Column(name = "apartment")
+    private String apartment;
+}
