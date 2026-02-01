d59e4c8a0a93085e87df23802fcca5b1446bb20f
emyca <evmyca@gmail.com>
2026-02-01 11:30:33 +0200

    Update Residence entity

--- src/main/java/com/example/Spring_Boot_Jpa_ER/_03_1_to_1_unidir_spk_pkjc/entity/Residence.java
+++ src/main/java/com/example/Spring_Boot_Jpa_ER/_03_1_to_1_unidir_spk_pkjc/entity/Residence.java
@@ -1,15 +1,13 @@
 package com.example.Spring_Boot_Jpa_ER._03_1_to_1_unidir_spk_pkjc.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
+@Builder
 @Entity(name = "residences")
 public class Residence {
     @Id
