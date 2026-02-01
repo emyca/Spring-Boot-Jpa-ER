80b3cb271dcc09a242ec5076a817b584d80d4a55
emyca <evmyca@gmail.com>
2026-02-01 11:37:10 +0200

    Update Habitation entity

--- src/main/java/com/example/Spring_Boot_Jpa_ER/_04_1_to_1_bidir_spk_pkjc/entity/Habitation.java
+++ src/main/java/com/example/Spring_Boot_Jpa_ER/_04_1_to_1_bidir_spk_pkjc/entity/Habitation.java
@@ -2,15 +2,13 @@ package com.example.Spring_Boot_Jpa_ER._04_1_to_1_bidir_spk_pkjc.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
+@Builder
 @Entity(name = "habitations")
 public class Habitation {
     @Id
