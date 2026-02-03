935c08f3f5c4b5565b440fa57a08af8208de6bf9
emyca <evmyca@gmail.com>
2026-02-03 14:38:28 +0200

    Update Comment entity

--- src/main/java/com/example/Spring_Boot_Jpa_ER/_08_1_to_N_unidir_fk_jc/entity/Comment.java
+++ src/main/java/com/example/Spring_Boot_Jpa_ER/_08_1_to_N_unidir_fk_jc/entity/Comment.java
@@ -1,15 +1,13 @@
 package com.example.Spring_Boot_Jpa_ER._08_1_to_N_unidir_fk_jc.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
+@Builder
 @Entity(name = "comments")
 public class Comment {
     @Id
