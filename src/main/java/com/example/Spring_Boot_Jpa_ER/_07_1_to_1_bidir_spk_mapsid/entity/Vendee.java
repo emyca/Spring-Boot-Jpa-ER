14f8bcaf9f9df6a7b386f8058a39699aec78e180
emyca <evmyca@gmail.com>
2026-02-03 14:02:57 +0200

    Update Vendee entity

--- src/main/java/com/example/Spring_Boot_Jpa_ER/_07_1_to_1_bidir_spk_mapsid/entity/Vendee.java
+++ src/main/java/com/example/Spring_Boot_Jpa_ER/_07_1_to_1_bidir_spk_mapsid/entity/Vendee.java
@@ -1,15 +1,13 @@
 package com.example.Spring_Boot_Jpa_ER._07_1_to_1_bidir_spk_mapsid.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
+@Builder
 @Entity(name = "vendees")
 public class Vendee {
     @Id
